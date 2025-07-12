d144e8ed2f730cb6826bd081fa1aa641c7f220c4
Sintayehu  Bikila <sench729@gmail.com>
2025-07-12 18:57:47 +0300

    feat: helper function

---
+++ src/helper.py
@@ -0,0 +1,26 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+
+
+def load_txt(data):
+    loader = DirectoryLoader(data,
+                             glob="*.txt",
+                             loader_cls=TextLoader)
+    documents = loader.load()
+    return documents
+
+
+
+def text_split(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 20)
+    text_chunks = text_splitter.split_documents(extracted_data)
+
+    return text_chunks
+
+# download embedding model
+
+def download_hugging_face_embeddings():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+    return embeddings
