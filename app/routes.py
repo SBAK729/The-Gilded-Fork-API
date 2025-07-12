69cd06c034d9f99dcbb1884849f06d611e89a627
Sintayehu  Bikila <sench729@gmail.com>
2025-07-12 19:00:30 +0300

    feat: completing chat endpoint for  route

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Request
+from pydantic import BaseModel
+from app.chat import get_chat_response
+
+router = APIRouter()
+
+class ChatRequest(BaseModel):
+    user_query: str
+
+@router.post("/chat")
+def chat(request: ChatRequest):
+  question = request.user_query.strip()
+  print(question)
+  
+  if not question:
+        return {"error": "Question cannot be empty."}
+  try:
+        response = get_chat_response(question)
+        print(response)
+        return {"answer": response}
+  except Exception as e:
+        print(e)
+        return {"error": "Something went wrong. Please try again."}
